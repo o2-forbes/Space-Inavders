e5ca9106e8450fa72a3409c9d3fb7567802f4077
Ollie <oliver2.forbes@live.uwe.ac.uk>
2024-03-12 13:10:14 +0000

    Update enemy.js

--- space_invaders_template/enemy.js
+++ space_invaders_template/enemy.js
@@ -1,3 +1,15 @@
-class Enemy extends GameObject {
+class Enemy extends GameObject 
+{
+    isDead = false;
+
+    draw()
+  {
+    fill(0,191,255);
+    rect(this.x(random(100)), this.y(random(100)), this.width(200), this.height(40));
+  }
+
+  move() 
+  {
     
+  }
 }
